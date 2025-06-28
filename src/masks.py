1edf8fa0845b0c341d5bfa582a168fa08484344d
Rim Faritov <faritov.rome@gmail.com>
2025-06-28 17:43:31 +0500

    fixes in modules in src

--- src/masks.py
+++ src/masks.py
@@ -2,9 +2,9 @@ def get_mask_card_number(card_num: str) -> str:
     """Функция для маскировки номера карты"""
     if len(card_num) == 16 and card_num.isdigit():
         return f"{card_num[:4]} {card_num[4:6]}** **** {card_num[12:]}"
-    if not card_num.isdigit():
+    elif not card_num.isdigit():
         return "Введены символы, некорректные для номера карты"
-    if len(card_num) > 16 or len(card_num) < 16:
+    else:
         return "Недостаток или избыток символов, проверьте количество введенных символов"
 
 
@@ -12,7 +12,7 @@ def get_mask_account(acc_num: str) -> str:
     """Функция для маскировки номера аккаунта"""
     if len(acc_num) == 20 and acc_num.isdigit():
         return f"**{acc_num[16:]}"
-    if not acc_num.isdigit():
-        return "Введены символы, некорректные для номера карты"
-    if len(acc_num) > 20 or len(acc_num) < 20:
+    elif not acc_num.isdigit():
+        return "Введены символы, некорректные для номера счета"
+    else:
         return "Недостаток или избыток символов, проверьте количество введенных символов"
--- src/widget.py
+++ src/widget.py
@@ -1,14 +1,21 @@
 from masks import get_mask_account, get_mask_card_number
 
+
 def mask_account_card(data_acc_or_card: str) -> str:
     "Функция, маскирующая номер счета или номер карты"
+    masked_data = " "
     if data_acc_or_card.startswith("Счет") and len(data_acc_or_card) == 25:
         masked_data = get_mask_account(data_acc_or_card[5:])
-    else:
+    elif data_acc_or_card[:-17].isalpha() and data_acc_or_card[-16:].isdigit() and len(data_acc_or_card[-16:]) == 16:
         masked_data = get_mask_card_number(data_acc_or_card[-16:])
     return masked_data
-# data_acc_or_card = "Счет 64686473678894779589"
-data_acc_or_card = "MasterCard 7158300734726758"
-# result = mask_account_card(data_acc_or_card)
-result = data_acc_or_card[-16:]
-print(result)
+
+
+def get_date(str_with_date_and_time: str) -> str:
+    "Функция, возвращающая дату в формате ДД.ММ.ГГГГ"
+    day_mon_year = " "
+    if len(str_with_date_and_time) == 26 and "T" in str_with_date_and_time:
+        day_mon_year = f"{str_with_date_and_time[8:10]}.{str_with_date_and_time[5:7]}.{str_with_date_and_time[0:4]}"
+    else:
+        "Некорректный ввод"
+    return day_mon_year
