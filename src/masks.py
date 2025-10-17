8b81d514eeefd5eb9b0433da81a5c023e9a3d8c0
Rim Faritov <faritov.rome@gmail.com>
2025-10-17 16:53:27 +0500

    add fixes to utils.py and to masks.py

--- src/masks.py
+++ src/masks.py
@@ -2,15 +2,43 @@ import logging
 import os
 
 
+def setup_logging_masks() -> None:
+    masks_logger.setLevel(logging.DEBUG)
+    path_to_masks = os.path.abspath(os.path.join(os.pardir, "logs", "masks.log"))
+
+    masks_file_handler = logging.FileHandler(path_to_masks, mode="w")
+    masks_file_formatter = logging.Formatter("%(asctime)s %(filename)s %(levelname)s: %(message)s")
+    masks_file_handler.setFormatter(masks_file_formatter)
+    masks_logger.addHandler(masks_file_handler)
+
+    masks_logger.debug("Debug message")
+    masks_logger.info("Info message")
+    masks_logger.warning("Warning message")
+    masks_logger.error("Error message")
+    masks_logger.critical("Critical message")
+
+# Вызов функции настройки логирования
+setup_logging_masks()
+
+# Настройка логгера
+masks_logger = logging.getLogger("masks")
+
 def get_mask_card_number(card_num: str) -> str:
     """Функция для маскировки номера карты"""
-    if len(card_num) == 16 and card_num.isdigit():
-        return f"{card_num[:4]} {card_num[4:6]}** **** {card_num[12:]}"
-    elif len(card_num) != 16:
-        return "Недостаток или избыток символов, проверьте количество введенных символов"
-    elif not card_num.isdigit():
-        return "Введены символы, некорректные для номера карты"
+    masks_logger.debug(f"Начало работы функции с параметром: {card_num}")
 
+    if len(card_num) == 16 and card_num.isdigit():
+        masked_card = f"{card_num[:4]} {card_num[4:6]}** **** {card_num[12:]}"
+        masks_logger.info(f"Маскированный номер карты: {masked_card}")
+        return masked_card
+    elif len(card_num) != 16:
+        warning_message = "Недостаток или избыток символов, проверьте количество введенных символов"
+        masks_logger.warning(warning_message)
+        return warning_message
+    elif not card_num.isdigit():
+        error_message = "Введены символы, некорректные для номера карты"
+        masks_logger.error(error_message)
+        return error_message
     else:
         return "Некорректный ввод"
 
@@ -18,30 +46,16 @@ def get_mask_card_number(card_num: str) -> str:
 def get_mask_account(acc_num: str) -> str:
     """Функция для маскировки номера аккаунта"""
     if len(acc_num) == 20 and acc_num.isdigit():
-        return f"**{acc_num[16:]}"
+        masked_acc = f"**{acc_num[16:]}"
+        masks_logger.info(f"Маскированный номер счета: {masked_acc}")
+        return masked_acc
     elif len(acc_num) != 20:
-        return "Недостаток или избыток символов, проверьте их количество"
+        warning_message = "Недостаток или избыток символов, проверьте их количество"
+        masks_logger.warning(warning_message)
+        return warning_message
     elif not acc_num.isdigit():
-        return "Введены символы, некорректные для номера счета"
-
+        error_message = "Введены символы, некорректные для номера счета"
+        masks_logger.error(error_message)
+        return error_message
     else:
         return "Некорректный ввод"
-
-
-root_logger = logging.getLogger()
-
-masks_logger = logging.getLogger("__main__")
-masks_logger.setLevel(logging.DEBUG)
-path_to_masks = os.path.abspath(os.path.join(os.pardir, "logs", "masks.log"))
-
-masks_file_handler = logging.FileHandler(path_to_masks, mode="w")
-masks_file_formatter = logging.Formatter("%(asctime)s %(filename)s %(levelname)s: %(message)s")
-masks_file_handler.setFormatter(masks_file_formatter)
-masks_logger.addHandler(masks_file_handler)
-masks_logger.setLevel(logging.DEBUG)
-
-masks_logger.debug("Debug message")
-masks_logger.info("Info message")
-masks_logger.warning("Warning message")
-masks_logger.error("Error message")
-masks_logger.critical("Critical message")
--- src/utils.py
+++ src/utils.py
@@ -3,35 +3,49 @@ import logging
 import os
 
 
+def setup_logging_utils() -> None:
+
+    utils_logger.setLevel(logging.DEBUG)
+    path_to_utils = os.path.abspath(os.path.join(os.pardir, "logs", "utils.log"))
+
+    utils_file_handler = logging.FileHandler(path_to_utils, mode="w")
+    utils_file_formatter = logging.Formatter("%(asctime)s %(filename)s %(levelname)s: %(message)s")
+    utils_file_handler.setFormatter(utils_file_formatter)
+    utils_logger.addHandler(utils_file_handler)
+
+    utils_logger.debug("Debug message")
+    utils_logger.info("Info message")
+    utils_logger.warning("Warning message")
+    utils_logger.error("Error message")
+    utils_logger.critical("Critical message")
+
+
+# Вызов функции настройки логирования
+setup_logging_utils()
+
+# Настройка логгера
+utils_logger = logging.getLogger("utils")
+
+
 def transactions_data(path_to_json_file: str) -> list:
     """Преобразование JSON-файла в список словарей"""
 
+    utils_logger.debug(f"Начало работы функции с параметром: {path_to_json_file}")
     try:
         with open(path_to_json_file, "r", encoding="utf-8") as f:
+            utils_logger.info(f"Чтение данных из файла{path_to_json_file}")
             transaction_data = json.load(f)
         if type(transaction_data) is not list or len(transaction_data) == 0:
+            utils_logger.warning("Данные не являются списком или список пуст")
             return []
+
+        utils_logger.info("Данные успешно преобразованы в список")
         return transaction_data
 
-    except json.JSONDecodeError:
+    except json.JSONDecodeError as e:
+        utils_logger.error(f"Ошибка декодирования JSON: {e}")
         return []
 
-    except FileNotFoundError:
+    except FileNotFoundError as e:
+        utils_logger.error(f"Файл не найден: {e}")
         return []
-
-
-utils_logger = logging.getLogger("utils")
-utils_logger.setLevel(logging.DEBUG)
-path_to_utils = os.path.abspath(os.path.join(os.pardir, "logs", "utils.log"))
-
-utils_file_handler = logging.FileHandler(path_to_utils, mode="w")
-utils_file_formatter = logging.Formatter("%(asctime)s %(filename)s %(levelname)s: %(message)s")
-utils_file_handler.setFormatter(utils_file_formatter)
-utils_logger.addHandler(utils_file_handler)
-utils_logger.setLevel(logging.DEBUG)
-
-utils_logger.debug("Debug message")
-utils_logger.info("Info message")
-utils_logger.warning("Warning message")
-utils_logger.error("Error message")
-utils_logger.critical("Critical message")
